2c5a9025b66dcb4980b9acdd70be3fa1d47a4882
mr.mitternacht <jamison.proctor@gmail.com>
2023-09-19 15:09:02 +0200

    fix typos in product description

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ if st.session_state.get("processing", False):
 if not st.session_state.initialized:
     st.title("easyGPTzee 🍋")
     st.markdown('''
-    easyGPTzy 🍋 is a user-friendly tool designed to streamline interactions with ChatGPT-4.0 and similar AI text interfaces. With the ability to dynamically generate intuitive UI widgets on the fly, it demystifies AI conversations, making them more accessible to everyone, regardless of their familiarity with prompt engineering or the realm of AI. 
+    easyGPTzee 🍋 is a user-friendly tool designed to streamline interactions with ChatGPT and similar AI text interfaces. With the ability to dynamically generate intuitive UI widgets on the fly, it demystifies AI conversations, making them more accessible to everyone, regardless of their familiarity with prompt engineering or the realm of AI. 
     
     🙏 Please understand that none of this content beyond this page is hardcoded, so please give the program time to process 🙏 
     ''')
